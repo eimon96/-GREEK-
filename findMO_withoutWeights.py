6f8ddae513b04a6a81579bebed96f3769556c3e4
Penny Simitsi <18515231+eimon96@users.noreply.github.com>
2021-07-05 18:54:21 +0300

    Updated - no weights

---
+++ findMO_withoutWeights.py
@@ -0,0 +1,46 @@
+# author: eimon
+
+def main():
+    try:
+        fp = open('meta.txt', 'r', encoding='utf-8')
+        grades = getGrades(fp)
+        fp.close()
+    except FileNotFoundError:
+        print("File Not Found.")
+    except:
+        print("An error occurred")
+
+    mo = getMO(grades)
+    print("Mesos Oros: {:4.3}".format(mo))
+
+
+
+def getGrades(myFile):
+    elements = []
+    grades = []
+
+    for line in myFile:
+        elements.append(line.split())
+
+    for element in elements:
+        for e in element:
+            try:
+                e = float(e)
+                grades.append(e)
+            except:
+                continue
+
+    return grades
+
+
+
+def getMO(grades):
+    arithmitis = 0
+    for grade in grades:
+        arithmitis += float(grade)
+
+    return (arithmitis/len(grades))
+
+
+
+main()
